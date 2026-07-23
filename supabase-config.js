cfbedd7b53377151fdc7726a7c651ae599e14c9e
melnik-av <andreyname@gmail.com>
2026-07-23 08:46:32 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // ⚠️ ЗАМЕНИТЕ НА ВАШИ ДАННЫЕ ИЗ SUPABASE
 // Settings → API → Project URL
-const SUPABASE_URL = 'https://hpisnwhoqzjkcmmlkmys.supabase.co/rest/v1';
+const SUPABASE_URL = 'https://hpisnwhoqzjkcmmlkmys.supabase.co';
 
 // Settings → API → anon public
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwaXNud2hvcXpqa2NtbWxrbXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MTU5ODIsImV4cCI6MjEwMDI5MTk4Mn0.qDsx5Hx7OioDSNSYIuyXkM7KAraBYV0cluOHciGRwY8';
